df008b8de82d13bfd24842841c49edda5ad396d0
VaibhaviAnnam <vaibhaviannam7@gmail.com>
2026-07-20 23:05:34 +0530

    Comparing strings

---
+++ comparison.java
@@ -0,0 +1,11 @@
+public class comparsion{
+    
+    public static void main(String args[]){
+        String str ="Vaibhavi";
+        String str1 = "Vaibhavi";
+        String str2 = "College";
+        System.out.println(str.equals(str1)+ " " + str.equals(str2));
+        String str3 = "ApnaCollege".replace("l", "0");
+        System.out.println(str3);
+    }
+}
